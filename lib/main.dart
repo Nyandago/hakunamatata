40d84696ed4ac39d657ae9d1348374c4b7ad6b30
nyandago <paulmutator@gmail.com>
2021-11-08 17:01:04 +0300

    Tap activation

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,15 @@ class _RandomWordsState extends State<RandomWords> {
         color: alreadySaved ? Colors.red : null,
         semanticLabel: alreadySaved ? 'Remove from Saved' : 'Save',
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
